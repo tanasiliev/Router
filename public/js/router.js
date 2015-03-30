54e3e3c5a722cbc91a98a3693ff5db630cf39e18
tanasiliev <the_wayfarer@abv.bg>
2015-03-30 15:17:22 +0300

    second commit

--- public/js/router.js
+++ public/js/router.js
@@ -1,1 +1,9 @@
-console.log("router")
+var fun = function(){
+	console.log(location.pathname)
+}
+window.addEventListener("load",fun,false)
+
+window.addEventListener('popstate', function(event) {
+  console.log('popstate fired!');
+   console.log(event.state);
+},false);
