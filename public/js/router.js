da1af2942288b4d518681b6e5339908d770b9f8e
Atanas Iliev <tanasiliev@gmail.com>
2015-03-31 12:21:28 +0300

    Update router.js

--- public/js/router.js
+++ public/js/router.js
@@ -1,9 +1,99 @@
-var fun = function(){
-	console.log(location.pathname)
-}
-window.addEventListener("load",fun,false)
+(function(){
+   
+  var utils = {
+      String : {
+          is : function(obj){
+              return  typeof obj == 'string' || obj instanceof String;    
+          },
+          isEmpty : function(obj){
+             return (obj.length === 0 || !obj.trim());
+          },
+          areEquals : function(str1, str2){
+             return str1.trim().toUpperCase() === str2.trim().toUpperCase();
+          }
+      },
+      Function : {
+            is : function(obj){
+              return !!(obj && obj.constructor && obj.call);     
+            }
+      },
+      error : {
+          show : function(msg){
+             throw new Error(msg);
+          }        
+      }
+  };    
+    
+  var Router = {
+      routes : [],
+      route : function(path, handler){
+         if(!utils.String.is(path) || utils.String.isEmpty(path)){
+             utils.error.show("Invalid path " + path);
+         }
+         if(!utils.Function.is(handler)){
+            utils.error.show("Invalid Function " + handler);
+         }    
+         var route = new Route(path, handler);
+         this.routes.push(route);    
+      },
+      run : function(){
+          var self = this;
+          var listener = function(event) {
+            self.findPath(location.pathname, event.state || undefined );  
+          }
+          window.addEventListener("load",listener, false);
+          window.addEventListener("popstate", listener, false);
+      },
+      findPath : function(url, state){
+          var routes = this.routes;   
+          for(var i = 0;  i < routes.length; i++ ){
+              var route = routes[i];
+              if(route.match(url)){
+                   route.handler(route.params || undefined, state);
+                   return;
+              }
+          }
+      }
+  };
+    
+  var Route = function(path, handler){
+      this.path = path;
+      this.handler = handler;
+  };
+  
+  Route.prototype.match = function(url){
+        var selt =  this;
+        if(utils.String.areEquals(selt.path, url)){
+           return true;
+        }
+        var params = selt.path.match(/\/:([^/]+)/gi);
+        if(params){
+            var pattern = selt.path;
+            for(var i =0; i < params.length; i++){
+                pattern = pattern.replace(params[i].slice(1), "([^/]+)");
+            }
+            var regex = new RegExp(pattern);
+            if(regex.exec(url)){
+               var obj = {};    
+               for(var i = 0; i < params.length; i++){
+                  var par = params[i];
+                  obj[par.slice(2)] = RegExp["$" + (i + 1)];
+               }    
+               selt.params = obj;
+               return true;    
+            }      
+        } 
+        return false;
+  };
 
-window.addEventListener('popstate', function(event) {
-  console.log('popstate fired!');
-   console.log(event.state);
-},false);
+   
+  window.Router = {
+      route: (function(){
+             Router.run();
+             return Router.route.bind(Router);
+      })()
+  }
+      
+})();
+
+
