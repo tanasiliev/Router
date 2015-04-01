184c6df1131b27c51f60438c0ffceb95d15a29a5
Atanas Iliev <tanasiliev@gmail.com>
2015-04-01 12:21:30 +0300

    Update router.js

--- public/js/router.js
+++ public/js/router.js
@@ -1,61 +1,89 @@
 (function(global){
-   
+    
    var utils = {
       String : {
-          areEquals : function(str1, str2){
-             return str1.trim().toUpperCase() === str2.trim().toUpperCase();
-          },
-          is : function(obj){
-              return  typeof obj == 'string' || obj instanceof String;    
-          },
-          isEmpty : function(obj){
-             return (obj.length === 0 || !obj.trim());
-          }
+         areEquals : function(str1, str2){
+            return str1.trim().toUpperCase() === str2.trim().toUpperCase();
+         },
+         is : function(val){
+            return  typeof val == 'string' || val instanceof String;    
+         },
+         isEmpty : function(val){
+            return (val.length === 0 || !val.trim());
+         }
       },
       Function : {
-            is : function(obj){
-              return !!(obj && obj.constructor && obj.call);     
-            }
+         is : function(val){
+           return !!(val && val.constructor && val.call);     
+         }
       },
+      Object: {
+         is: function(val){
+            return Object.prototype.toString.apply(val) === "[object Object]"; 
+         },
+         isEmpty: function(val) {
+            return Object.keys(val).length === 0;
+         }    
+      },   
       Error : {
-          throw : function(msg){
-             throw new Error(msg);
-          }        
+         throw : function(msg){
+            throw new Error(msg);
+         }        
       }
    };    
     
    var Router = {
       routes : [],
-      route : function(path, handler){
+      defaultRoute: null,
+      route: function(a, b){
+         var self = this;  
+         if(arguments.length == 1){
+            var routes = arguments[0];
+            if(!utils.Object.is(routes) || utils.Object.isEmpty(routes)){
+               utils.Error.throw("An not empty object is required!");
+            }
+            for(var key in routes){
+               self._route(key, routes[key]);
+            }
+            return;
+         }
+         self._route.apply(self, arguments); 
+      },      
+      _route : function(path, handler){
          if(!utils.String.is(path) || utils.String.isEmpty(path)){
-             utils.Error.throw("Invalid path " + path);
+            utils.Error.throw("Invalid path " + path);
          }
          if(!utils.Function.is(handler)){
             utils.Error.throw("Invalid Function " + handler);
-         }    
+         }
+         if(utils.String.areEquals(path, "*")){
+            this.defaultRoute = handler;
+            return;  
+         }       
          var route = new Route(path, handler);
          this.routes.push(route);    
       },
       run : function(){
-          var self = this;
-          var listener = function(event) {
+         var self = this;
+         var listener = function(event) {
             self.findRoute(location.pathname, event.state);  
-          }
-          window.addEventListener("load",listener, false);
-          window.addEventListener("popstate", listener, false);
+         }
+         window.addEventListener("load",listener, false);
+         window.addEventListener("popstate", listener, false);
       },
       findRoute : function(url, state){
-          var routes = this.routes;   
-          for(var i = 0;  i < routes.length; i++ ){
-              var route = routes[i];
-              if(route.match(url)){
-                   var options = { 
-                       params: route.params, 
-                       state : state 
-                   };
-                   return route.handler(options);
-              }
-          }
+         var routes = this.routes;   
+         for(var i = 0;  i < routes.length; i++ ){
+            var route = routes[i];
+            if(route.match(url)){
+               var options = { 
+                  params: route.params, 
+                  state : state 
+               };
+               return route.handler(options);
+            }
+         }
+         this.defaultRoute && this.defaultRoute();
       }
    };
     
@@ -99,9 +127,7 @@
    if(typeof define === 'function' && define.amd){ 
       define(function () { return router; });
    } else { 
-      global['observer'] = router;
+      global['router'] = router;
    }
     
 })(this);
-
-
